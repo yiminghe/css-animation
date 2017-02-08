f4caed5e018fabe90690ce7607bbc4d0da5816b4
yiminghe <yiminghe@gmail.com>
2017-02-08 20:41:11 +0800

    fix for old ff. close #10

--- src/index.js
+++ src/index.js
@@ -10,8 +10,8 @@ const capitalPrefixes = ['Webkit',
 const prefixes = ['-webkit-', '-moz-', '-o-', 'ms-', ''];
 
 function getStyleProperty(node, name) {
-  const style = window.getComputedStyle(node);
-
+  // old ff need null, https://developer.mozilla.org/en-US/docs/Web/API/Window/getComputedStyle
+  const style = window.getComputedStyle(node, null);
   let ret = '';
   for (let i = 0; i < prefixes.length; i++) {
     ret = style.getPropertyValue(prefixes[i] + name);
