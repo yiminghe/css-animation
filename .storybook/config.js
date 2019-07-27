88eb4180872792ce4d1b66829fc0b816954983a0
yiminghe <yiminghe@gmail.com>
2019-07-28 02:02:33 +0800

    up

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,13 @@
-import { configure } from '@storybook/react';
+import { addParameters, configure } from '@storybook/react';
+
+addParameters({
+  options: {
+    theme: {
+      brandTitle: 'css-animation',
+      brandUrl: 'https://github.com/yiminghe/css-animation/',
+    }
+  }
+});
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /.js$/);
